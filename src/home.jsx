b59ef0aa89dad718314d007efa928b3915aa2fe6
varun-developer-public <varundev.bytes@gmail.com>
2025-07-21 22:56:33 +0530

    `Added dynamic product rendering using API data and React hooks`

--- src/home.jsx
+++ src/home.jsx
@@ -1,14 +1,23 @@
+import { useEffect, useState } from 'react'
 import ProductCard from './components/productCard/productCard'
 
 function Home() {
+  const [products,setProducts] = useState([])
+  useEffect(()=>{
+    fetch("https://dummyjson.com/products")
+    .then(res=>res.json())
+    .then(data=>setProducts(data.products))
+  },[])
+  console.log(products);
+  
   return (
     <div>
         <h1>Home Page</h1>
-        <ProductCard imglink={"https://rukminim2.flixcart.com/image/832/832/xif0q/mobile/n/l/u/-original-imah2fjd7wfd9ksh.jpeg?q=70&crop=false"} title="Title 1" price={53200}/>
-        <ProductCard imglink={"https://rukminim2.flixcart.com/image/832/832/xif0q/mobile/n/l/u/-original-imah2fjd7wfd9ksh.jpeg?q=70&crop=false"} title="Title 2" price={8484}/>
-        <ProductCard imglink={"https://rukminim2.flixcart.com/image/832/832/xif0q/mobile/n/l/u/-original-imah2fjd7wfd9ksh.jpeg?q=70&crop=false"} title="Title 3" price={4484}/>
-        <ProductCard imglink={"https://rukminim2.flixcart.com/image/832/832/xif0q/mobile/n/l/u/-original-imah2fjd7wfd9ksh.jpeg?q=70&crop=false"} title="Title 4" price={85522}/>
-
+        {
+          products.map((items)=>{
+            return <ProductCard key={items.id} imglink={items.thumbnail} title={items.title} price={items.price}/>
+          })
+        }
     </div>
   )
 }
