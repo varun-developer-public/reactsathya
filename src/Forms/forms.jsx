8a90e4f8c1aefc696c33f62df811f8dbfa23a5ea
varun-developer-public <varundev.bytes@gmail.com>
2025-07-25 09:10:19 +0530

    `Added Tailwind CSS and styled-components to the project, updated package-lock.json and package.json, and modified Vite configuration and React component to use the new dependencies.`

---
+++ src/Forms/forms.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import styles from'./forms.module.css'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #BF4F74;
+  color: #BF4F74;
+  margin: 0 1em;
+  padding: 0.25em 1em;`
+
+function RegistrationForms() {
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`Form submitted with data:
+        First Name: ${formData.firstName}`)
+    }
+    
+    const handleChange = (event) => {
+        const {name,value} = event.target
+        setFormData(()=>({
+            ...formData,
+            [name]: value
+        }))
+    }
+    let fontSize = 20
+
+  return (
+    <>
+        <h1 className="p-4 bg-gray-100 rounded shadow">Registration Forms</h1>
+
+        <Button>Click Me</Button>
+        <form onSubmit={handleSubmit} noValidate>
+            <label htmlFor="fname">First Name:</label>
+            <input type="text" id="fname" name="firstName" value={formData.firstName} onChange={handleChange}/>
+            <br /> <br />
+                <label htmlFor="lname">Last Name:</label>
+                <input type="text" id="lname" name="lastName" value={formData.lastName} onChange={handleChange}/>
+                <br /> <br />
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange}/>
+            <br /> <br />
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" value={formData.password} onChange={handleChange}/>
+            {(formData.password.length<3|| formData.password.length >8) && <span style={{fontSize:`${fontSize}px`}}>invalid password</span>}
+            {(formData.password.length<3|| formData.password.length >8) && <span style={{fontSize:`${fontSize}px`}}>invalid password</span>}
+            {(formData.password.length<3|| formData.password.length >8) && <span style={{fontSize:`${fontSize}px`}}>invalid password</span>}
+            {(formData.password.length<3|| formData.password.length >8) && <span style={{fontSize:`${fontSize}px`}}>invalid password</span>}
+            {(formData.password.length<3|| formData.password.length >8) && <span style={{fontSize:`${fontSize}px`}}>invalid password</span>}
+            {(formData.password.length<3|| formData.password.length >8) && <span style={{fontSize:`${fontSize}px`}}>invalid password</span>}
+            <br /> <br />
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input type="password" id="confirmPassword" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange}/>
+            {
+                formData.password!=formData.confirmPassword && <span>PASSWORD NOT MATCHING</span>
+            }
+            <br /> <br />
+            <button type="submit">Register</button>
+            <button type="reset" className=''>Reset</button>
+            </form>
+    </>
+  )
+}
+
+export default RegistrationForms
--- src/home.jsx
+++ src/home.jsx
@@ -1,23 +1,27 @@
 import { useEffect, useState } from 'react'
 import ProductCard from './components/productCard/productCard'
+import RegistrationForms from './Forms/forms'
 
 function Home() {
-  const [products,setProducts] = useState([])
-  useEffect(()=>{
-    fetch("https://dummyjson.com/products")
-    .then(res=>res.json())
-    .then(data=>setProducts(data.products))
-  },[])
-  console.log(products);
   
+
+  // const [products,setProducts] = useState([])
+  // useEffect(()=>{
+  //   fetch("https://dummyjson.com/products")
+  //   .then(res=>res.json())
+  //   .then(data=>setProducts(data.products))
+  // },[products])
+  // console.log(products);
+
   return (
     <div>
         <h1>Home Page</h1>
-        {
+        {/* {
           products.map((items)=>{
             return <ProductCard key={items.id} imglink={items.thumbnail} title={items.title} price={items.price}/>
           })
-        }
+        } */}
+        <RegistrationForms/>
     </div>
   )
 }
